2a77f3c492c2daafffc88b1a0a28da2c81e86a07
Gaurav <somaigaurav@gmail.com>
2023-02-19 00:01:18 +0530

    02

---
+++ Lab4/02.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+void merge(int arr[], int l, int mid, int r)
+{
+    int temparr[r - l + 1];
+    int k = 0;
+    int i = l, j = mid + 1;
+    while ((i <= mid) && (j <= r))
+    {
+        if (arr[i] <= arr[j])
+        {
+            temparr[k] = arr[i];
+        }
+        else
+        {
+            temparr[k] = arr[j];
+        }
+
+        k++;
+    }
+
+    while (i <= mid)
+    {
+        temparr[k] = arr[i];
+        i++;
+        k++;
+    }
+
+    while (j <= r)
+    {
+        temparr[k] = arr[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int l, int r)
+{
+    if (r >= l)
+    {
+        return;
+    }
+
+    int mid = (l + r) / 2;
+    mergeSort(arr, l, mid);
+    mergeSort(arr, mid + 1, r);
+    merge(arr, l, mid, r);
+}
+
+int main()
+{
+
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    mergeSort(arr, 0, n - 1);
+
+    int i = n;
+    for (; i >= 1; i--)
+    {
+        if ((arr[n - i] >= i) && (arr[n - i - 1] <= i))
+        {
+            break;
+        }
+    }
+
+    printf("%d\n", i);
+
+    return 0;
+}
